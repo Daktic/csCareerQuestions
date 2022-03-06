3755177e219d742b0c00bb770dc53d3b0942ef45
Daktic <58493066+Daktic@users.noreply.github.com>
2022-03-05 23:19:23 -0500

    refactor to class

--- scrape.js
+++ scrape.js
@@ -1,8 +1,55 @@
 const { r } = require('./connection.js');
 
 
-r.getTop('cscareerquestions',{
-    time: 'all',
-    limit: 10
-}).then(console.log);
+
+//create class to create new postGuildTokens object
+class submissionCoinage {
+    constructor(subreddit, numOfSubmissions, timeFrame) {
+        this._subreddit = subreddit;
+        this._numOfSubmissions = numOfSubmissions;
+        this._timeFrame = timeFrame;
+        this._postGildTokens = {
+            'url': '',
+            'coinValue': 0
+        }
+    } // I want to have a new ob
+
+    get subreddit() {
+        return this._subreddit;
+    }
+    get numOfSubmissions() {
+        return this._numOfSubmissions;
+    }
+    get timeFrame() {
+        return this._timeFrame;
+    }
+
+    //this will need to be new class and this function wall call that class
+    getSubData() {
+        let subreddit = this._subreddit
+        const subReddit = r.getSubreddit(subreddit)
+        //get top subreddit by time range, limit number of submissions to retrieve
+        subReddit.getTop({
+            time: this._timeFrame,
+            limit: this._numOfSubmissions
+        }).then((posts) => {
+            posts.forEach(post => {
+        
+                postGildTokens.url = post.url;
+                post.all_awardings.forEach(
+                    award => {
+                        postGildTokens.coinValue += award.coin_price * award.count //counts coinvalue times num times given
+                    }
+                )
+                console.log(postGildTokens);
+            })
+        });
+    }
+}
+
+
+
+todayCsCoin = new submissionCoinage('cscareerquestions', 1, 'week');
+
+console.log(todayCsCoin.getSubData());
 
